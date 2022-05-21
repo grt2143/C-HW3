cd0b8b259d2068dc600254f093cb04b399add7da
Alex <alexchurilov@mail.ru>
2022-05-21 18:26:22 +0300

    Create Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите значение:");
+string value = Console.ReadLine();
+char [] arr = value.ToCharArray();
+Array.Reverse(arr);
+string endValue = new string(arr);
+if(value == endValue)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
