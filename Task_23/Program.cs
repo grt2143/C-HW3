f605fa1c586d446380f205a4de793aaf62236ec2
Alex <alexchurilov@mail.ru>
2022-05-21 20:53:19 +0300

    Add Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая принимает на вход число 
+// и выдает таблицу кубов чисел от 1 до этого числа
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+    Console.WriteLine($"Число{count, 10} -> Куб числа{Math.Pow(count, 3), 10}");
+    count++;
+}
